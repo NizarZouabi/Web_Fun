6749cdecde3f97988baaf6888c84c4a025446e43
Nizar Zouabi <136129653+NizarZouabi@users.noreply.github.com>
2024-08-05 05:45:58 +0100

    #

--- JS/Pacman/script.js
+++ JS/Pacman/script.js
@@ -5,10 +5,10 @@ var map = [
     [2,1,2,1,1,1,1,1,1,1,2,1,2,1,2,1,2,1,2,2,1,2,1,2,1,2,2,1,2,1,1,2,1,2],
     [2,1,1,1,2,2,2,2,2,1,1,1,1,1,2,1,2,1,2,1,1,2,1,2,1,1,2,1,2,1,1,2,1,2],
     [2,1,2,1,1,1,1,1,1,1,2,1,2,1,2,1,2,1,2,1,2,2,1,2,2,1,2,2,2,1,1,2,1,2],
-    [2,1,2,2,2,2,1,2,2,2,2,1,2,1,2,1,1,1,2,1,2,1,1,1,2,1,1,1,2,1,2,2,2,2],
-    [0,1,1,1,1,1,1,1,1,1,1,1,2,1,2,1,2,1,2,1,2,1,2,1,2,2,2,1,1,1,2,1,1,0],
+    [2,1,2,2,2,2,1,2,2,2,2,1,2,1,2,1,1,1,2,1,2,1,1,1,2,1,1,1,1,1,2,2,2,2],
+    [-3,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,2,1,2,1,2,1,2,1,2,1,2,2,2,1,1,1,1,3],
     [2,1,2,2,2,2,1,2,2,2,2,1,2,1,2,1,2,1,2,2,2,1,2,1,1,1,2,1,2,1,1,1,2,2],
-    [2,1,1,1,1,2,1,2,1,1,2,1,2,1,2,1,2,1,2,1,1,1,2,2,2,1,2,1,2,2,2,2,2,2],
+    [2,1,1,1,1,2,1,2,1,1,2,1,2,1,2,1,2,1,1,1,1,1,2,2,2,1,2,1,2,2,2,2,2,2],
     [2,1,2,2,1,2,1,2,1,2,2,1,2,1,1,1,2,1,2,2,2,1,1,1,2,1,2,1,1,1,1,1,1,2],
     [2,1,2,1,1,1,1,2,1,2,1,1,2,1,2,1,2,1,2,1,2,1,2,2,2,1,2,1,2,2,2,2,1,2],
     [2,1,2,1,2,2,2,2,1,2,1,2,2,2,2,1,2,2,2,1,2,1,2,1,2,1,2,2,2,1,1,2,1,2],
@@ -17,6 +17,11 @@ var map = [
 ];
 
 var pacman = {
+    x: 1,
+    y: 1
+}
+
+var ghost = {
     x: 1,
     y: 1
 }
@@ -38,7 +43,7 @@ function displayMap(){
             } else if(map[i][j] == 1){
                 output += "\n\t<div class='coin'></div>"
             }
-            if(map[i][j] == 0){
+            if(map[i][j] == 0 || map[i][j] == 3 || map[i][j] == -3){
                 output += "\n\t<div class='empty'></div>"
             }
         }
@@ -59,10 +64,12 @@ displayPacman()
 document.onkeydown = function(e){
 
     if(e.key === "ArrowRight" && map[pacman.y][pacman.x + 1] != 2){
+        document.getElementById('pac-man').style.transform = 'scaleX(1)'
         pacman.x += 1
     } else if(e.key === "ArrowDown" && map[pacman.y + 1][pacman.x] != 2){
         pacman.y += 1
     } else if(e.key === "ArrowLeft" && map[pacman.y][pacman.x - 1] != 2){
+        document.getElementById('pac-man').style.transform = 'scaleX(-1)'
         pacman.x -= 1
     } else if(e.key === "ArrowUp" && map[pacman.y - 1][pacman.x] != 2){
         pacman.y -= 1
@@ -74,6 +81,14 @@ document.onkeydown = function(e){
 
         displayScore()
         displayMap()
+    }
+
+    if(map[pacman.y][pacman.x] === 3){
+        pacman.y = 7
+        pacman.x = 1
+    } else if (map[pacman.y][pacman.x] === -3){
+        pacman.y = 7
+        pacman.x = 32
     }
 
     displayPacman()
